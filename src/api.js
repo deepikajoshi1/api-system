108f6d6a5f09b59c89a1ad4264e07fe18bc6fcc4
deepika joshi <upreti.deepika@gmail.com>
2023-05-20 16:25:02 +0800

    added async and await keywords before the request to hadle the request correctly

--- src/api.js
+++ src/api.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
-const searchImages = () => {
-axios.get('https://api.unsplash.com/search/photos',{
+const searchImages = async () => {
+await axios.get('https://api.unsplash.com/search/photos',{
   headers: {
     Authorization: 'Client-ID GZPE1xjJA6_H4a_Sagy5VFcfwAwTqJf-IyktSZS-QJQ'
 
