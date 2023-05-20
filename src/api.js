8b96beebe547d1ba3210b2e6e90e81751f70ce03
deepika joshi <upreti.deepika@gmail.com>
2023-05-20 16:37:22 +0800

    assigened the http request to a variale response and then return the variable respone

--- src/api.js
+++ src/api.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 const searchImages = async () => {
-await axios.get('https://api.unsplash.com/search/photos',{
+const response = await axios.get('https://api.unsplash.com/search/photos',{
   headers: {
     Authorization: 'Client-ID GZPE1xjJA6_H4a_Sagy5VFcfwAwTqJf-IyktSZS-QJQ'
 
@@ -10,6 +10,6 @@ await axios.get('https://api.unsplash.com/search/photos',{
 
   }
 });
-
+return response;
 };
 export default searchImages;
