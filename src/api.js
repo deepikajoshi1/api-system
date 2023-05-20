fe9a5f80ea770e1affacbf796881bad14cb0406d
deepika joshi <upreti.deepika@gmail.com>
2023-05-20 16:18:44 +0800

    made get request with axios

--- src/api.js
+++ src/api.js
@@ -1,5 +1,15 @@
 import axios from 'axios';
-const searchImages (){
+const searchImages = () => {
+axios.get('https://api.unsplash.com/search/photos',{
+  headers: {
+    Authorization: 'Client-ID GZPE1xjJA6_H4a_Sagy5VFcfwAwTqJf-IyktSZS-QJQ'
 
-}
+  },
+  params: {
+    query:'dogs'
+
+  }
+});
+
+};
 export default searchImages;
