a0235e1740b654040364dbb506cff4067c4df9b2
deepika joshi <upreti.deepika@gmail.com>
2023-05-22 15:59:09 +0800

    added api keys in the env file

--- src/api.js
+++ src/api.js
@@ -1,10 +1,10 @@
 import axios from 'axios';
 const searchImages = async (term) => {
 const response = await axios.get('https://api.unsplash.com/search/photos',{
-  headers: {
-    Authorization: 'Client-ID GZPE1xjJA6_H4a_Sagy5VFcfwAwTqJf-IyktSZS-QJQ',
+  // headers: {
+  //   Authorization: 'Client-ID GZPE1xjJA6_H4a_Sagy5VFcfwAwTqJf-IyktSZS-QJQ',
 
-  },
+  // },
   params: {
     query:term
 
