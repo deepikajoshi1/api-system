1327bc4bd506eac6de9abe47457aea2f9949a208
deepika joshi <upreti.deepika@gmail.com>
2023-05-22 15:04:17 +0800

    added term in the argument list and in the query, updated the return statement with response.data.results

--- src/api.js
+++ src/api.js
@@ -1,15 +1,16 @@
 import axios from 'axios';
-const searchImages = async () => {
+const searchImages = async (term) => {
 const response = await axios.get('https://api.unsplash.com/search/photos',{
   headers: {
-    Authorization: 'Client-ID GZPE1xjJA6_H4a_Sagy5VFcfwAwTqJf-IyktSZS-QJQ'
+    Authorization: 'Client-ID GZPE1xjJA6_H4a_Sagy5VFcfwAwTqJf-IyktSZS-QJQ',
 
   },
   params: {
-    query:'dogs'
+    query:term
 
   }
 });
-return response;
+console.log(response);
+return response.data.results;
 };
 export default searchImages;
--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
-import searchImages from './api';
+
 // Get a reference to the div with ID root:
 const element = document.getElementById('root');
 // Tell React to take control of that element:
