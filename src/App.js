94edaa2d26603ed499dbf7e097bdfb410781891c
deepika joshi <upreti.deepika@gmail.com>
2023-05-23 15:57:53 +0800

    added handleSubmit function in app.js

--- src/App.js
+++ src/App.js
@@ -1,16 +1,15 @@
 // Create a component: A component is a function that returns JSX.
-import searchImages from './api';
 import SearchBar from './components/SearchBar';
 
-function App(){
+function App() {
   const handleSubmit = (term) => {
     console.log('Do a search with', term);
-    searchImages(term)
+
 
   };
   return (
     <div>
-      <SearchBar onSubmit={handleSubmit}/>
+      <SearchBar onSubmit={handleSubmit} />
     </div>
   );
 }
