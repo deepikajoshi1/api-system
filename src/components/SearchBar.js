99ffb1dfba3757d1da31e14e3826e427bb635d84
deepika joshi <upreti.deepika@gmail.com>
2023-05-23 16:27:31 +0800

    added handleChange function with event input and call the set term

--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -1,9 +1,12 @@
 import { useState } from 'react';
 function SearchBar({ onSubmit }) {
-  const[term, setTerm] = useState('');
+  const [term, setTerm] = useState('');
   const handleFormSubmit = (event) => {
     event.preventDefault();
     onSubmit('dogs');
+  };
+  const handleChange = (event) => {
+    setTerm(event.target.value);
   };
   return (
     <div>
