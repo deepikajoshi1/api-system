3158f2274cc70d15c2f61a350b6c069f5b2e1ed2
deepika joshi <upreti.deepika@gmail.com>
2023-05-23 16:19:22 +0800

    added term and setTerm function

--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -1,5 +1,6 @@
 import { useState } from 'react';
 function SearchBar({ onSubmit }) {
+  const[term, setTerm] = useState('');
   const handleFormSubmit = (event) => {
     event.preventDefault();
     onSubmit('dogs');
