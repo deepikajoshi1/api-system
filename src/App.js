7ee8a678941337bfa5dcd474afef44c9e0aab663
deepika joshi <upreti.deepika@gmail.com>
2023-05-24 10:00:47 +0800

    imported ImageList in app.js file

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 // Create a component: A component is a function that returns JSX.
 import {useState} from 'react';
 import SearchBar from './components/SearchBar';
+import ImageList from './components/ImageList';
 
 
 function App() {
